e308dc240f9d0cab42e508926058d4b081ce545e
Your GitHub Username <Your GitHub Email>
2023-11-27 09:01:01 +0100

    create a "routers" directory to keep the app as cleaner as possible

--- app/main.py
+++ app/main.py
@@ -1,26 +1,15 @@
-from sqlalchemy import func
-from sqlalchemy.orm import Session
-
-from .models import Memory
-from . import models, schemas, utils
-from .database import engine, get_db
-
 import openai
 import uvicorn
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-# from typing import List
-import time
-
-from dotenv import load_dotenv, find_dotenv
-import json
 import os
 import psycopg2
-
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
-
 import secrets
+import time
+from fastapi import FastAPI, status
+from . import models
+from .database import engine
+from dotenv import load_dotenv, find_dotenv
+from .routers import conversation, user
+
 
 _ = load_dotenv(find_dotenv())
 
@@ -37,11 +26,6 @@ app.secret_key = secret_key
 # Heroku provides the DATABASE_URL environment variable
 DATABASE_URL = os.environ['DATABASE_URL']
 
-# Initialize an empty conversation chain
-llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0301")  # Set your desired LLM model here
-memory = ConversationBufferMemory()
-conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
-memory_summary = ConversationSummaryBufferMemory(llm=llm, max_token_limit=19)
 
 while True:
     try:
@@ -82,170 +66,13 @@ def find_index_converse(id):
     return None
 
 
-# Function to generate LLM response
-def generate_llm_response(user_message):
-    # Assuming 'conversation' is initialized as a ChatOpenAI object
-    return conversation.predict(input=user_message)
+app.include_router(conversation.router)
+app.include_router(user.router)
 
 
 @app.get("/", status_code=status.HTTP_201_CREATED)
 async def root():
-    return {"message: Be Good Doing Good By Acting Good ¡!¡": "Siisi Chacal 🔥👌🏿😇💪🏿🔥"}
-
-
-@app.get("/all-conversation", status_code=status.HTTP_201_CREATED)
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM memories""")
-    # posts = cursor.fetchall()(db: Session = Depends(get_db)):
-    histories = db.query(models.Memory).all()
-    print(f'all-conversation:\n{histories} 👌🏿\n')
-    return histories
-
-
-@app.get("/conversation-summary", status_code=status.HTTP_201_CREATED)
-def get_conversation_summary(db: Session = Depends(get_db)):
-    conversation_summaries_all = [summary.conversations_summary for summary in db.query(models.Memory).all()]
-    head_summaries = conversation_summaries_all[:3]
-    tail_summaries = conversation_summaries_all[-3:]
-
-    print(f'conversation_summaries_head:\n{head_summaries}\n\n'
-          f'conversation_summaries_tail:\n{tail_summaries} 👌🏿\n')
-
-    return {"conversation_summary_head": head_summaries, "conversation_summary_tail": tail_summaries}
-
-
-@app.post("/start-conversation", status_code=status.HTTP_201_CREATED, response_model=schemas.MemoryCreate)
-def start_conversation(omr: schemas.MemoryCreate, db: Session = Depends(get_db)):
-    try:
-        # Use SQLAlchemy ORM to insert a new record
-        new_memo = Memory(**omr.model_dump())
-
-        # Get LLM response using the user's message
-        llm_response = generate_llm_response(omr.user_message)
-
-        # Update the new_memo with the LLM response
-        new_memo.llm_response = llm_response
-
-        # Save the conversation context
-        memory_summary.save_context({"input": omr.user_message}, {"output": llm_response})
-
-        # Load the conversation context
-        conversations_summary = memory_summary.load_memory_variables({})
-
-        # Convert the dictionary to a JSON-formatted string
-        conversations_summary_json = json.dumps(conversations_summary)
-
-        print(f'omr.user_message:\n{omr.user_message}\n')
-        print(f'llm_response:\n{llm_response}\n')
-        print(f'conversations_summary:\n{conversations_summary_json}\n')
-
-        # Set the conversation summary in the new_memo
-        new_memo.conversations_summary = conversations_summary_json
-
-        # Set the timestamp using the database server's time
-        new_memo.created_at = func.now()
-
-        db.add(new_memo)
-        db.commit()
-        db.refresh(new_memo)
-
-        return new_memo
-    except Exception as e:
-        # Log the exception or print it for debugging
-        print(f"An error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-
-@app.get("/audio", status_code=status.HTTP_201_CREATED)
-async def audio_response():
-    print(f'audio_response:\n{conversations_datas[-1:]}\n')
-    return {"message: Be Good Doing Good By Acting Good ¡!¡": conversations_datas[-1:]}
-
-
-@app.get("/conversation_by_id/{id}", status_code=status.HTTP_201_CREATED, response_model=schemas.MemoryResponse)
-def get_conversation_by_id(id: int, db: Session = Depends(get_db)):
-    converse = db.query(models.Memory).filter(models.Memory.id == id).first()
-    if not converse:
-        print(f'conversations_by_id: "{id}" was not found')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"conversations_by_id: '{id}' was not found'")
-
-    # Use Pydantic's model.dict() to convert the SQLAlchemy model to a dictionary
-    converse_dict = schemas.MemoryResponse(**converse.__dict__).model_dump()
-    print(f'get_conversation_by_id:\n{converse_dict}\n')
-
-    return converse_dict
-
-
-@app.put("/update-conversation/{id}", response_model=schemas.MemoryResponse)
-def upd_conversation(id: int, updated_memory: schemas.MemoryResponse, db: Session = Depends(get_db)):
-    # Check if the conversation exists in the database
-    existing_memory_query = db.query(models.Memory).filter(models.Memory.id == id)
-    existing_memory = existing_memory_query.first()
-
-    if existing_memory is None:
-        print(f'Conversation with ID: {id} does not exist')
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Conversation with ID: {id} does not exist"
-        )
-
-    # Update the existing_memory with the values from updated_memory
-    for field in updated_memory.model_dump().keys():
-        if hasattr(existing_memory, field):
-            setattr(existing_memory, field, getattr(updated_memory, field))
-
-    db.commit()
-
-    # Use Pydantic's model.dict() to convert the SQLAlchemy model to a dictionary
-    memory_dict = schemas.MemoryResponse(**updated_memory.__dict__).model_dump()
-
-    # Logging the conversation update
-    print(f'Conversation with ID: {id} updated:\n{memory_dict}')
-    return memory_dict
-
-
-@app.delete("/delete-conversation/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def del_conversation(id: int, db: Session = Depends(get_db)):
-    del_converse = db.query(models.Memory).filter(models.Memory.id == id)
-
-    if del_converse.first() is None:
-        print(f'conversations_by_id: "{id}" does not exist')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"conversation with id: '{id}' does not exist")
-    del_converse.delete(synchronize_session=False)
-    db.commit()
-    print(f'"message": "conversation was successfully deleted"')
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # Hash the password - user.password
-    hashed_password = utils.hash_(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    print(f'new_user:\nEmail: {new_user.email}\nPassword: {new_user.password}\n')
-
-    return new_user
-
-
-@app.get('/users/{id}', response_model=schemas.UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        print(f'User with id: {id} does not exist')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id: {id} does not exist')
-
-    print(f'id: {id}\nuser email: {user.email}\nuser password: {user.password}\n'
-          f'user created_at: {user.created_at}')
-
-    return user
+    return {"Be Good Doing Good By Acting Good ¡!¡": "Siisi Chacal 🔥👌🏿😇💪🏿🔥"}
 
 
 if __name__ == "__main__":
---
+++ app/routers/conversation.py
@@ -0,0 +1,155 @@
+from fastapi import Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+import json
+from ..models import Memory
+from .. import models, schemas
+from ..database import get_db
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
+
+
+router = APIRouter(
+    prefix="/conversation",
+    tags=['Conversations']
+)
+
+# Initialize an empty conversation chain
+llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0301")  # Set your desired LLM model here
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
+memory_summary = ConversationSummaryBufferMemory(llm=llm, max_token_limit=19)
+
+
+# Function to generate LLM response
+def generate_llm_response(user_message):
+    # Assuming 'conversation' is initialized as a ChatOpenAI object
+    return conversation.predict(input=user_message)
+
+
+@router.get("/all", status_code=status.HTTP_201_CREATED)
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM memories""")
+    # posts = cursor.fetchall()(db: Session = Depends(get_db)):
+    histories = db.query(models.Memory).all()
+    print(f'all conversation:\n{histories} 👌🏿\n')
+    return histories
+
+
+@router.get("/summary", status_code=status.HTTP_201_CREATED)
+def get_conversation_summary(db: Session = Depends(get_db)):
+    conversation_summaries_all = [summary.conversations_summary for summary in db.query(models.Memory).all()]
+    head_summaries = conversation_summaries_all[:3]
+    tail_summaries = conversation_summaries_all[-3:]
+
+    print(f'Conversation Summary Head:\n{head_summaries}\n\n'
+          f'Conversation Summary Tail:\n{tail_summaries} 👌🏿\n')
+
+    return {"Conversation Summary Head": head_summaries, "Conversation Summary Tail": tail_summaries}
+
+
+@router.post("/start", status_code=status.HTTP_201_CREATED, response_model=schemas.MemoryCreate)
+def start_conversation(omr: schemas.MemoryCreate, db: Session = Depends(get_db)):
+    try:
+        # Use SQLAlchemy ORM to insert a new record
+        new_memo = Memory(**omr.model_dump())
+
+        # Get LLM response using the user's message
+        llm_response = generate_llm_response(omr.user_message)
+
+        # Update the new_memo with the LLM response
+        new_memo.llm_response = llm_response
+
+        # Save the conversation context
+        memory_summary.save_context({"input": omr.user_message}, {"output": llm_response})
+
+        # Load the conversation context
+        conversations_summary = memory_summary.load_memory_variables({})
+
+        # Convert the dictionary to a JSON-formatted string
+        conversations_summary_json = json.dumps(conversations_summary)
+
+        print(f'omr.user_message:\n{omr.user_message}\n')
+        print(f'llm_response:\n{llm_response}\n')
+        print(f'conversations_summary:\n{conversations_summary_json}\n')
+
+        # Set the conversation summary in the new_memo
+        new_memo.conversations_summary = conversations_summary_json
+
+        # Set the timestamp using the database server's time
+        new_memo.created_at = func.now()
+
+        #db.add(new_memo)
+        #db.commit()
+        #db.refresh(new_memo)
+
+        return new_memo
+    except Exception as e:
+        # Log the exception or print it for debugging
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@router.get("/audio", status_code=status.HTTP_201_CREATED)
+async def audio_response(db: Session = Depends(get_db)):
+    audio = db.query(models.Memory).all()
+    print(f'audio response:\n{audio[-1:]}\n')
+    return {"message: Be Good Doing Good By Acting Good ¡!¡": audio[-1:]}
+
+
+@router.get("/get/{id}", status_code=status.HTTP_201_CREATED, response_model=schemas.MemoryResponse)
+def get_conversation_by_id(id: int, db: Session = Depends(get_db)):
+    converse = db.query(models.Memory).filter(models.Memory.id == id).first()
+    if not converse:
+        print(f'conversations with ID: "{id}" was not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"conversations with ID: '{id}' was not found'")
+
+    # Use Pydantic's model.dict() to convert the SQLAlchemy model to a dictionary
+    converse_dict = schemas.MemoryResponse(**converse.__dict__).model_dump()
+    print(f'get conversation with ID:\n{converse_dict}\n')
+
+    return converse_dict
+
+
+@router.put("/update/{id}", response_model=schemas.MemoryResponse)
+def upd_conversation(id: int, updated_memory: schemas.MemoryResponse, db: Session = Depends(get_db)):
+    # Check if the conversation exists in the database
+    existing_memory_query = db.query(models.Memory).filter(models.Memory.id == id)
+    existing_memory = existing_memory_query.first()
+
+    if existing_memory is None:
+        print(f'Conversation with ID: {id} does not exist')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Conversation with ID: {id} does not exist"
+        )
+
+    # Update the existing_memory with the values from updated_memory
+    for field in updated_memory.model_dump().keys():
+        if hasattr(existing_memory, field):
+            setattr(existing_memory, field, getattr(updated_memory, field))
+
+    db.commit()
+
+    # Use Pydantic's model.dict() to convert the SQLAlchemy model to a dictionary
+    memory_dict = schemas.MemoryResponse(**updated_memory.__dict__).model_dump()
+
+    # Logging the conversation update
+    print(f'Conversation with ID: {id} updated:\n{memory_dict}')
+    return memory_dict
+
+
+@router.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def del_conversation(id: int, db: Session = Depends(get_db)):
+    del_converse = db.query(models.Memory).filter(models.Memory.id == id)
+
+    if del_converse.first() is None:
+        print(f'conversations with ID: "{id}" does not exist')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"conversation with ID: '{id}' does not exist")
+    del_converse.delete(synchronize_session=False)
+    db.commit()
+    print(f'"message": "conversation was successfully deleted"')
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
