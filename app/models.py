433a07343a9151fcd97cef3eb992756c5002e06f
Your GitHub Username <Your GitHub Email>
2023-11-29 10:21:35 +0100

    add a "owner_id" column with a foreignkey for the user table

--- app/models.py
+++ app/models.py
@@ -1,19 +1,22 @@
-from sqlalchemy import Column, INTEGER, String, Boolean, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 from app.database import Base
 
 
 class Memory(Base):
     __tablename__ = 'omr'
-    id = Column(INTEGER, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, server_default=text('1'))
     user_message = Column(String, nullable=False)
     llm_response = Column(String, nullable=False)
     conversations_summary = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
-    rating = Integer()
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.utcnow)
+
+    owner_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
 
 
 class User(Base):
@@ -21,5 +24,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.utcnow)
