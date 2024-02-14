91272a83e8832c7f7512d83b15a42f982d8f490d
Your GitHub Username <Your GitHub Email>
2024-02-14 18:09:51 +0100

    another try

--- app/database.py
+++ app/database.py
@@ -19,10 +19,9 @@ load_dotenv()
 # )
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{os.environ['USER']}:{os.environ['PASSWORD']}@"
-    f"{os.environ['HOST']}:{os.environ['PORT']}/{os.environ['DATABASE']}"
+    f"postgresql://{os.environ['user']}:{os.environ['password']}@"
+    f"{os.environ['host']}:{os.environ['port']}/{os.environ['database']}"
 )
-
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
