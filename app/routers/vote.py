354b46b3fc4521d9429d00b1c3145b30ee96a63a
Your GitHub Username <Your GitHub Email>
2024-02-15 01:02:11 +0100

    use different approch to handle the case if the voted post does not exist

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from .. import schemas, database, models, oauth2
 
+
 router = APIRouter(
     prefix="/vote",
     tags=['Vote']
@@ -21,26 +22,19 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db),
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id,
                                               models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
-    
-    try:
-        if vote.dir == 1:
-            if found_vote:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                    detail=f"user {current_user.id} has already voted on post {vote.post_id}")
-            new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
-            db.add(new_vote)
-            db.commit()
-            return {"message": "successfully added vote"}
-        else:
-            if not found_vote:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
-            
-            vote_query.delete(synchronize_session=False)
-            db.commit()
+    if (vote.dir == 1):
+        if found_vote:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"user {current_user.id} has already voted on post {vote.post_id}")
+        new_vote = models.Vote(post_id = vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "successfully added vote"}
+    else:
+        if not found_vote:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
+        
+        vote_query.delete(synchronize_session=False)
+        db.commit()
 
-            return {"message": "successfully deleted vote"}
-
-    # Handle the IntegrityError and raise a 404 Not Found
-    except IntegrityError as e:
-        db.rollback()
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Vote {vote.post_id} does not exist")
+        return {"message": "successfully deleted vote"}
