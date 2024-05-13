7d6438ca3ad460fb070114791cf417520815a2ee
Your GitHub Username <Your GitHub Email>
2024-05-13 19:56:49 +0200

    to locate .env file

--- app/database.py
+++ app/database.py
@@ -2,13 +2,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import psycopg2
 import time
+from dotenv import load_dotenv, find_dotenv
 
 
-load_dotenv()
-
+_ = load_dotenv(find_dotenv())
 
 # from .config import settings
 
