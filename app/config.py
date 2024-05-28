07b4fafa2b2a48bf9d16cbc5f49661f30905af4a
Your GitHub Username <Your GitHub Email>
2024-05-28 09:25:26 +0200

    update build_deploy.yml and .env file to fix github action error

--- app/config.py
+++ app/config.py
@@ -9,12 +9,12 @@ class Settings(BaseSettings):
     model_config = ConfigDict( env_file=".env")
 
     admin_host: str
-    admin_port: str
+    admin_port: int
     admin_password: str 
     admin_database: str
     admin_user: str 
     admin_algorithm: str 
-    admin_access_token_expire_minutes: str 
+    admin_access_token_expire_minutes: int 
     admin_database_url: str
     admin_oauth2_secret_key: str
     openai_api_key: str
