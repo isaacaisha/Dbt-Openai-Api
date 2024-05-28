aeef01d79c7c2ab1f4c7d00014979480f6a5f998
Your GitHub Username <Your GitHub Email>
2024-05-28 08:52:35 +0200

    update config.py to fix github action errors

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import ConfigDict
+from pydantic import ConfigDict, Field
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
@@ -9,12 +9,12 @@ class Settings(BaseSettings):
     model_config = ConfigDict( env_file=".env")
 
     admin_host: str
-    admin_port: int
+    admin_port: int = Field(default=5432, env='ADMIN_PORT')
     admin_password: str 
     admin_database: str
     admin_user: str 
     admin_algorithm: str 
-    admin_access_token_expire_minutes: int 
+    admin_access_token_expire_minutes: int = Field(default=55, env='ADMIN_ACCESS_TOKEN_EXPIRE_MINUTES') 
     admin_database_url: str
     admin_oauth2_secret_key: str
     openai_api_key: str
